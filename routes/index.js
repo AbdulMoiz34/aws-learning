9c79ed02e6367863cdb0a8ba881c7712c79a7d9e
AbdulMoiz34 <moiz05366@gmail.com>
2026-03-23 05:02:23 +0500

    add demo files

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,16 @@
 import { Router } from "express";
 import userRoutes from "./user.routes.js";
+import sum from "../sum.js";
 
 const router = Router();
 
-router.get("/", (req, res) => {
+router.get("/", (_, res) => {
     return res.status(200).send("Api is running");
+});
+
+router.get("/getSum/:a/:b", (req, res) => {
+    const { a, b } = req.params;
+    res.status(200).json({ data: sum(+a, +b) });
 });
 
 router.use("/users", userRoutes);
