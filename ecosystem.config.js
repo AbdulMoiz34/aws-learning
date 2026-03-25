4df4699e2576336815102356117a562fefbbfcb5
AbdulMoiz34 <moiz05366@gmail.com>
2026-03-25 19:34:30 +0500

    added ecosystem file and updated yml

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    apps: [
+        {
+            name: "app",
+            script: "./index.js",
+            instances: "max",
+            autorestart: true,
+            watch: false,
+            env: {
+                NODE_ENV: "production"
+            }
+        }
+    ]
+};
